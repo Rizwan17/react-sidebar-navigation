571ececadd23ef374d3127cd2b043668e434504d
Rizwan Khan <rizwankhan.august16@gmail.com>
2021-06-16 07:46:21 -0700

    react sidebar navigation initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
 import "./App.css";
-import SideMenu from "./components/SideMenu";
+import SideMenu, { menuItems } from "./components/SideMenu";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Content = () => <h1>Content</h1>;
+const Courses = () => <h1>Content/Courses</h1>;
+const Videos = () => <h1>Content/Videos</h1>;
+const Design = () => <h1>Design</h1>;
+const Content2 = () => <h1>Content2</h1>;
+const Courses2 = () => <h1>Content/Courses 2</h1>;
+const Videos2 = () => <h1>Content/Videos 2</h1>;
+const Design2 = () => <h1>Design 2</h1>;
 
 function App() {
+  const [inactive, setInactive] = useState(false);
+
   return (
     <div className="App">
-      <SideMenu />
+      <Router>
+        <SideMenu
+          onCollapse={(inactive) => {
+            console.log(inactive);
+            setInactive(inactive);
+          }}
+        />
+
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+          {/* improvememt, not recorded in video, its just looping through menuItems
+          instead of hard coding all the routes */}
+          {menuItems.map((menu, index) => (
+            <>
+              <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                <h1>{menu.name}</h1>
+              </Route>
+              {menu.subMenus && menu.subMenus.length > 0
+                ? menu.subMenus.map((subMenu, i) => (
+                    <Route key={subMenu.name} path={subMenu.to}>
+                      <h1>{subMenu.name}</h1>
+                    </Route>
+                  ))
+                : null}
+            </>
+          ))}
+
+          {/* <Switch>
+            <Route exact path={"/"}>
+              <Dashboard />
+            </Route>
+            <Route exact path={"/content"}>
+              <Content />
+            </Route>
+            <Route path={"/content/courses"}>
+              <Courses />
+            </Route>
+            <Route path={"/content/videos"}>
+              <Videos />
+            </Route>
+            <Route path={"/design"}>
+              <Design />
+            </Route>
+            <Route exact path={"/content-2"}>
+              <Content2 />
+            </Route>
+            <Route path={"/content-2/courses"}>
+              <Courses2 />
+            </Route>
+            <Route path={"/content-2/videos"}>
+              <Videos2 />
+            </Route>
+            <Route path={"/design-2"}>
+              <Design2 />
+            </Route>
+          </Switch> */}
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,14 +1,84 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo/webscript.png";
 import user from "../assets/user.jpg";
+
+import MenuItem from "./MenuItem";
 
 /**
  * @author
  * @function SideMenu
  **/
 
+// added more menuItems for testing
+export const menuItems = [
+  {
+    name: "Dashboard",
+    exact: true,
+    to: "/",
+    iconClassName: "bi bi-speedometer2",
+  },
+  {
+    name: "Content",
+    exact: true,
+    to: `/content`,
+    iconClassName: "bi bi-speedometer2",
+    subMenus: [
+      { name: "Courses", to: "/content/courses" },
+      { name: "Videos", to: "/content/videos" },
+    ],
+  },
+  { name: "Design", to: `/design`, iconClassName: "bi bi-vector-pen" },
+  {
+    name: "Content 2",
+    exact: true,
+    to: `/content-2`,
+    iconClassName: "bi bi-speedometer2",
+    subMenus: [
+      { name: "Courses", to: "/content-2/courses" },
+      { name: "Videos", to: "/content-2/videos" },
+    ],
+  },
+  { name: "Design 2", to: `/design-2`, iconClassName: "bi bi-vector-pen" },
+  { name: "Design 3", to: `/design-3`, iconClassName: "bi bi-vector-pen" },
+  { name: "Design 4", to: `/design-4`, iconClassName: "bi bi-vector-pen" },
+];
+
 const SideMenu = (props) => {
   const [inactive, setInactive] = useState(false);
+
+  useEffect(() => {
+    if (inactive) {
+      removeActiveClassFromSubMenu();
+    }
+
+    props.onCollapse(inactive);
+  }, [inactive]);
+
+  //just an improvment and it is not recorded in video :(
+  const removeActiveClassFromSubMenu = () => {
+    document.querySelectorAll(".sub-menu").forEach((el) => {
+      el.classList.remove("active");
+    });
+  };
+
+  /*just a little improvement over click function of menuItem
+    Now no need to use expand state variable in MenuItem component
+  */
+  useEffect(() => {
+    let menuItems = document.querySelectorAll(".menu-item");
+    menuItems.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        const next = el.nextElementSibling;
+        removeActiveClassFromSubMenu();
+        menuItems.forEach((el) => el.classList.remove("active"));
+        el.classList.toggle("active");
+        console.log(next);
+        if (next !== null) {
+          next.classList.toggle("active");
+        }
+      });
+    });
+  }, []);
 
   return (
     <div className={`side-menu ${inactive ? "inactive" : ""}`}>
@@ -37,7 +107,23 @@ const SideMenu = (props) => {
 
       <div className="main-menu">
         <ul>
-          <li>
+          {menuItems.map((menuItem, index) => (
+            <MenuItem
+              key={index}
+              name={menuItem.name}
+              exact={menuItem.exact}
+              to={menuItem.to}
+              subMenus={menuItem.subMenus || []}
+              iconClassName={menuItem.iconClassName}
+              onClick={(e) => {
+                if (inactive) {
+                  setInactive(false);
+                }
+              }}
+            />
+          ))}
+
+          {/* <li>
             <a className="menu-item">
               <div className="menu-icon">
                 <i class="bi bi-speedometer2"></i>
@@ -45,22 +131,10 @@ const SideMenu = (props) => {
               <span>Dashboard</span>
             </a>
           </li>
-          <li>
-            <a className="menu-item">
-              <div className="menu-icon">
-                <i class="bi bi-newspaper"></i>
-              </div>
-              <span>Content</span>
-            </a>
-            <ul className="sub-menu">
-              <li>
-                <a>Courses</a>
-              </li>
-              <li>
-                <a>Videos</a>
-              </li>
-            </ul>
-          </li>
+          <MenuItem
+            name={"Content"}
+            subMenus={[{ name: "Courses" }, { name: "Videos" }]}
+          />
           <li>
             <a className="menu-item">
               <div className="menu-icon">
@@ -68,7 +142,7 @@ const SideMenu = (props) => {
               </div>
               <span>Design</span>
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
 
